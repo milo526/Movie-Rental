c8c5c2fa0fce045d86161206853859383f44bf31
Milo Cesar <milocesar1@gmail.com>
2016-02-14 16:33:40 +0100

    Changed pivot seeder
    ID integrity failed when working with new db

--- database/seeds/Actor_MovieTableSeeder.php
+++ database/seeds/Actor_MovieTableSeeder.php
@@ -23,17 +23,17 @@ class Actor_MovieTableSeeder extends Seeder
 
     	DB::table('actor_movie')->insert([
         	'actor_id' => 4,
-        	'movie_id' => 4,
+        	'movie_id' => 2,
     	]);
 
         DB::table('actor_movie')->insert([
         	'actor_id' => 1,
-        	'movie_id' => 5,
+        	'movie_id' => 3,
     	]);
 
     	DB::table('actor_movie')->insert([
         	'actor_id' => 2,
-        	'movie_id' => 5,
+        	'movie_id' => 3,
     	]);
     }
 }
