4dc58eac286e3c544b7f44804f0de10d4ad3882c
Milo Cesar <milocesar1@gmail.com>
2016-02-14 16:06:17 +0100

    Added actor-movie seeder

---
+++ database/seeds/Actor_MovieTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Actor_MovieTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	DB::table('actor_movie')->insert([
+        	'actor_id' => 3,
+        	'movie_id' => 1,
+    	]);
+
+    	DB::table('actor_movie')->insert([
+        	'actor_id' => 4,
+        	'movie_id' => 1,
+    	]);
+
+    	DB::table('actor_movie')->insert([
+        	'actor_id' => 4,
+        	'movie_id' => 4,
+    	]);
+
+        DB::table('actor_movie')->insert([
+        	'actor_id' => 1,
+        	'movie_id' => 5,
+    	]);
+
+    	DB::table('actor_movie')->insert([
+        	'actor_id' => 2,
+        	'movie_id' => 5,
+    	]);
+    }
+}
