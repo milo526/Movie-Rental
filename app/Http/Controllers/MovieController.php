16dddc0ddbcb5351190062bfcd1339ecbe4ebbb7
Milo Cesar <milocesar1@gmail.com>
2016-03-16 17:07:58 +0100

    Added movie controller

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use Tmdb\Helper\ImageHelper;
+use Tmdb\Repository\MovieRepository;
+
+class MovieController extends Controller{
+
+	private $movies;
+
+	public function __construct(MovieRepository $movies, ImageHelper $helper){
+        $this->movies = $movies;
+    }
+
+    function index($id)
+    {
+        // returns information of a movie
+        $movie = $this->movies->load($id);
+        return view('data/movie')->with('movie', $movie);
+    }
+}
