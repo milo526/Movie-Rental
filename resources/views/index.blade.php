8b9ba507fa7f09e3c2a996c2d5f1e2caa57192ed
Milo Cesar <milocesar1@gmail.com>
2016-03-19 16:54:13 +0100

    Changed index layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,14 +8,14 @@
 
     <div class="row row-margin-bottom">
         @foreach ($movies as $movie)
-            <div class="col-md-3 col-md-offset-1 col-sm-5 col-sm-offset-1 no-padding no-link lib-item" data-category="view">
+            <div class="col-sm-5 col-sm-offset-1 no-padding no-link lib-item" data-category="view">
                 <a href="/movie/{{ $movie->getId() }}">
                     <div class="lib-panel">
                         <div class="row box-shadow">
-                            <div class="col-md-12">
+                            <div class="col-md-6 col-sm-12">
                                 {!! $image->getHtml($movie->getPosterImage(), 'w780', 260, 420) !!}
                             </div>
-                            <div class="col-md-12">
+                            <div class="col-md-6 col-sm-12">
                                 <div class="lib-row lib-header">
                                     {{$movie->getTitle()}}
                                     <div class="lib-header-seperator"></div>
